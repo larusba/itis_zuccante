5082ce3160a2588ba5d7a025c265c69a019e9fc2
Kiren Borin <kiren@LARUSs-MBP-2.fritz.box>
2021-09-10 11:33:08 +0200

    Added FuelConsumption class

--- src/main/java/com/larus/itiszuccante/domain/CarType.java
+++ src/main/java/com/larus/itiszuccante/domain/CarType.java
@@ -2,19 +2,10 @@ package com.larus.itiszuccante.domain;
 
 public enum CarType {
 
-    SMALL(5.23), MID_SIZE(6.7), LUXURY_SUV_VAN(8.39);
+    SMALL, MID_SIZE, LUXURY_SUV_VAN;
 
     /**
      * Litres consumption for 100km
      */
-    private final double consumption;
-
-    CarType(double d) {
-        this.consumption = d;
-    }
-
-    public double getConsumption() {
-        return consumption;
-    }
 
 }
