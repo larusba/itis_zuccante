a248a7f44190c6edaa75735c3b625214bc4e5813
Davide Pizzato <davidepizzato@Stage03.homenet.telecomitalia.it>
2023-09-19 16:21:45 +0200

    added functionality to botton Show Intervention

--- ui/app/modules/hospitalInformation/hospitalInformation-screen.js
+++ ui/app/modules/hospitalInformation/hospitalInformation-screen.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Avatar, Button, Card, Text, Badge, Modal, Portal, PaperProvider } from 'react-native-paper';
+import { Avatar, Button, Card, Text, Badge, Modal, Portal, Divider } from 'react-native-paper';
 import config from './../../config/app-config.js';
+import { find } from 'lodash';
 
 export default function HospitaInformation() {
   const [listHAndI, setListHAndI] = useState([]);
   const [listInterventions, setListInterventions] = useState([]);
+  const [ospedale, setOspedale] = useState('');
   const images = [
     '',
     'https://www.aulss3.veneto.it/myportal/AU12VE/api/content/download?id=6321ac733b36fa00b9ca1c25',
@@ -37,7 +39,7 @@ export default function HospitaInformation() {
 
   useEffect(() => {
     (async () => {
-      const response = await fetch(`${config.apiUrl}api/findInterventionsByHospitalName?hospitalName=` + 'Ospedale%20di%20Mestre', {
+      const response = await fetch(`${config.apiUrl}api/findInterventionsByHospitalName?hospitalName=Ospedale%20di%20Mestre`, {
         headers: {
           Authorization:
             'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiYXV0aCI6IlJPTEVfQURNSU4sUk9MRV9VU0VSIiwiZXhwIjoxNjk3MjgxMzMyfQ.uETEWdLIwmUwKCvc28egLtnQOnd7Tm-Ky6bxGC2oz9pS_-8dAjlbWXc-X2RU8lSv9Z2rCMvPL1SynzzMoNUraw',
@@ -45,7 +47,6 @@ export default function HospitaInformation() {
       });
 
       setListInterventions(await response.json());
-      console.log(listInterventions);
     })();
   }, []);
 
@@ -64,11 +65,31 @@ export default function HospitaInformation() {
         />
         <Card.Actions>
           <Button disabled={hospitalIntervetion.countIntervention <= 0} onPress={showModal}>
-            Mostra interventi{' '}
+            Show Interventions
           </Button>
           <Badge size={45}>{hospitalIntervetion.countIntervention}</Badge>
         </Card.Actions>
       </Card>
+    );
+  }
+
+  function miaFunzione2(intervention, i) {
+    return (
+      <View key={i} style={styles.interventionsView}>
+        <Text variant="titleMedium">{i + 1}° Intervention</Text>
+        <Divider />
+        <Text>Patient name: {intervention.nomePaziente}</Text>
+        <Divider />
+        <Text>Patient surname: {intervention.cognomePaziente}</Text>
+        <Divider />
+        <Text>Ambulance plate: {intervention.numeroAmbulanza}</Text>
+        <Divider />
+        <Text>Intervention's address: {intervention.luogoIntervento}</Text>
+        <Divider />
+        <Text>latitude: {intervention.latitude}</Text>
+        <Divider />
+        <Text>longitude: {intervention.longitude}</Text>
+      </View>
     );
   }
 
@@ -80,9 +101,9 @@ export default function HospitaInformation() {
           onDismiss={hideModal}
           style={styles.modalStyle}
           contentContainerStyle={containerStyle}
-          theme={{ colors: { primary: 'green' } }}
+          //theme={{ height: { backdrop: 5000 } }}
         >
-          <Text>Example Modal. Click outside this area to dismiss.</Text>
+          <Text>{listInterventions.map(miaFunzione2)}</Text>
         </Modal>
       </Portal>
       {listHAndI.length > 0 && <View>{listHAndI.map(miaFunzione)}</View>}
@@ -96,5 +117,8 @@ const styles = StyleSheet.create({
   },
   modalStyle: {
     position: 'fixed',
+  },
+  interventionsView: {
+    padding: 5,
   },
 });
